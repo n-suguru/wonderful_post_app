458e767c6d1c15cd844059af2dde417a7edf83e5
西原卓 <r4ta160e@gmail.com>
2022-05-11 15:27:26 +0900

    検索フォームの実装 Tagのmodelとvalidationの実装 articleとtagのアソシエーションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :articles
+  get '/mypage', to: 'mypage#show'
   get 'users/index'
   get 'users/show'
   devise_for :users
   root to: 'articles#index'
-  resources :articles
-  resources :mypage
+  # resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
